23389aeeed885108e232b8650aceda1cc176fe6d
LStarQA77 <laura.starkand@gmail.com>
2026-07-07 15:41:42 -0700

    removing unnecessary test file that was incomplete

--- tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.integrated-concrete.com/');
-  await page.getByLabel('Site').getByRole('link', { name: 'Request A Quote' }).click();
-  await page.goto('https://www.integrated-concrete.com/');
-  await page.goto('https://www.integrated-concrete.com/');
-});
